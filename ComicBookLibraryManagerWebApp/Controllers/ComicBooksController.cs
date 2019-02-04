8728decd642d93205137ab7555fa7504910ae6c8
Joshua Dawson <josh@dawsonhosting.com>
2019-02-03 16:55:16 -0800

    Updated ComicBook Controller To Update and Delete

--- ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
+++ ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
@@ -80,6 +80,8 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 comicBook.AddArtist(viewModel.ArtistId, viewModel.RoleId);
 
                 // TODO Add the comic book.
+                _context.ComicBooks.Add(comicBook);
+                _context.SaveChanges();
 
                 TempData["Message"] = "Your comic book was successfully added!";
 
@@ -99,8 +101,9 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            // TODO Get the comic book.
-            var comicBook = new ComicBook();
+            var comicBook = _context.ComicBooks
+                .Where(cb => cb.Id == id)
+                .SingleOrDefault();
 
             if (comicBook == null)
             {
@@ -127,6 +130,9 @@ namespace ComicBookLibraryManagerWebApp.Controllers
 
                 // TODO Update the comic book.
 
+                _context.Entry(comicBook).State = EntityState.Modified;
+                _context.SaveChanges();
+
                 TempData["Message"] = "Your comic book was successfully updated!";
 
                 return RedirectToAction("Detail", new { id = comicBook.Id });
@@ -146,7 +152,10 @@ namespace ComicBookLibraryManagerWebApp.Controllers
 
             // TODO Get the comic book.
             // Include the "Series" navigation property.
-            var comicBook = new ComicBook();
+            var comicBook = _context.ComicBooks
+                .Include(cb => cb.Series)
+                .Where(cb => cb.Id == id)
+                .SingleOrDefault();
 
             if (comicBook == null)
             {
@@ -159,7 +168,10 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         [HttpPost]
         public ActionResult Delete(int id)
         {
-            // TODO Delete the comic book.
+            var comicBook = new ComicBook() { Id = id };
+            _context.Entry(comicBook).State = EntityState.Deleted;
+            _context.SaveChanges();
+
 
             TempData["Message"] = "Your comic book was successfully deleted!";
 
@@ -174,17 +186,20 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         private void ValidateComicBook(ComicBook comicBook)
         {
             //// If there aren't any "SeriesId" and "IssueNumber" field validation errors...
-            //if (ModelState.IsValidField("ComicBook.SeriesId") &&
-            //    ModelState.IsValidField("ComicBook.IssueNumber"))
-            //{
+            if (ModelState.IsValidField("ComicBook.SeriesId") &&
+                ModelState.IsValidField("ComicBook.IssueNumber"))
+            {
             //    // Then make sure that the provided issue number is unique for the provided series.
             //    // TODO Call method to check if the issue number is available for this comic book.
-            //    if (false)
-            //    {
-            //        ModelState.AddModelError("ComicBook.IssueNumber",
-            //            "The provided Issue Number has already been entered for the selected Series.");
-            //    }
-            //}
+                if (_context.ComicBooks
+                    .Any(cb => cb.Id != comicBook.Id &&
+                               cb.SeriesId == comicBook.SeriesId &&
+                               cb.IssueNumber == comicBook.IssueNumber))
+                {
+                    ModelState.AddModelError("ComicBook.IssueNumber",
+                        "The provided Issue Number has already been entered for the selected Series.");
+                }
+            }
         }
 
         private bool _disposed = false;
